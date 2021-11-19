dedad5d7e237a42d6b1974feda22561ab170c524
Baska Babicova <barb.babicova@gmail.com>
2021-11-19 10:24:42 +0100

    Init

---
+++ src/components/WelcomeBox.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="welcome-box">
+      <h2>Welcome to Weather-cloud!</h2>
+      <p>Hi, would you like to know, how weather looks like in your city?<br> Just type its name in the search bar.</p>
+  </div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+.welcome-box {
+  padding: 10px 25px;
+  color: #fff;
+  font-size: 40px;
+  font-weight: 900px;
+  text-align: center;
+  
+
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+  margin-left: auto;
+  margin-right: auto;
+
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+</style>
